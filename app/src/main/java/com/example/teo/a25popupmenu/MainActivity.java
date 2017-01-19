daee9eebcf27d6cdeb35c2c387a58d943ad24416
c Thun Nguyn <nguyenducthuan.cit@gmail.com>
2017-01-19 18:39:07 +0700

    Hoàn thành

--- app/src/main/java/com/example/teo/a25popupmenu/MainActivity.java
+++ app/src/main/java/com/example/teo/a25popupmenu/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.teo.a25popupmenu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Creating the instance of PopupMenu
+                PopupMenu popup = new PopupMenu(MainActivity.this, button1);
+                //Inflating the Popup using xml file
+                popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+
+                //registering popup with OnMenuItemClickListener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(MainActivity.this, "You Clcked :" + item.getTitle(), Toast.LENGTH_LONG).show();
+                        return true;
+                    }
+                });
+
+                popup.show(); //Showing popup menu
+            }
+        });
 
     }
 }
